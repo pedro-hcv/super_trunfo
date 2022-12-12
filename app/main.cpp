ae920a71e80f74a90eef659c86df60db83c9fe87
pedro-hcv <pedrohcv23@gmail.com>
2022-12-12 02:20:15 -0300

    versão final

--- app/main.cpp
+++ app/main.cpp
@@ -3,17 +3,4 @@
 int main(){
     //using namespace trabalho;
     trabalho::Interface Supertrunfo;
-    /* Baralho b1;
-    b1.criarCartas();
-    b1.embaralhar();
-    b1.status(); */
-    /* Jogo j1;
-    j1.distribuir_cartas();
-    //j1.status();
-    j1.inicio(); */
-//        std::shared_ptr<no> temp = lista_copia.cabeca;
-    //jogo = &Supertrunfo;
-    /* std::shared_ptr<Interface> jogo;
-    jogo->inicio(); */
-    //j1.status();
 }
